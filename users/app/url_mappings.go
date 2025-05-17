c205e2531dca29384deee97cbc5a9cebf93ad2a5
carohavenstein <carohavenstein@gmail.com>
2025-05-17 19:29:28 -0300

    funcion change password lista

--- users/app/url_mappings.go
+++ users/app/url_mappings.go
@@ -11,15 +11,18 @@ func mapUrls() {
 	router.POST("/register", userController.InsertUser)
 	router.POST("/login", userController.Login)
 
-	// Authenticated user - user is account owner (id = endpoint_id) or admin
+	// User is Authenticated and account owner (id = endpoint_id)
 	router.GET("/user/email/:email", userController.GetUserByEmail)
-	router.GET("/user/:id", userController.GetUserById)
 	router.PATCH("/user/:id", userController.UpdateUser)
+	router.PATCH("/user/:id/change-password", userController.ChangePassword)
+
+	// User is Authenticated and account owner (id = endpoint_id) or ADMIN
+	router.GET("/user/:id", userController.GetUserById)
 	//router.DELETE("/user/:id", userController.DeleteUser)
 
 	// Only admin
 	//router.GET("/reports", userController.ViewReports)
-	//router.PATCH("/suspend/:id", userController.SuspendUSer)
+	//router.PATCH("/suspend/:id", userController.SuspendUser)
 
 	log.Info("Url Mapping ready")
 }
