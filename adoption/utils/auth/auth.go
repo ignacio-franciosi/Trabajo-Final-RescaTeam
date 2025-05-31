e587e6fc5ad8b26aa3c2bb565f37d29805dd067d
ignacio-franciosi <2104338@ucc.edu.ar>
2025-05-31 11:48:53 -0300

    docker adoption

--- adoption/utils/auth/auth.go
+++ adoption/utils/auth/auth.go
@@ -38,6 +38,7 @@ func VerifyTokenAndAuthorize(c *gin.Context, allowAdmin bool, allowOwner bool) (
 	reqBody, _ := json.Marshal(TokenRequest{Token: token})
 
 	authUrl := "http://localhost:8082/verify"
+	//authUrl := "http://rescateam-auth:8082/verify" //docker
 	resp, err := http.Post(authUrl, "application/json", bytes.NewBuffer(reqBody))
 	if err != nil {
 		fmt.Println("===> Error al llamar al auth:", err)
