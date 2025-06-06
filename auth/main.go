25bf74419ae3d31197f5b482bf5ef11b97516cbe
ignacio-franciosi <2104338@ucc.edu.ar>
2025-06-06 20:09:23 -0300

    cambio en puerto de auth

--- auth/main.go
+++ auth/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"auth/middleware"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,7 +10,15 @@ import (
 func main() {
 	r := gin.Default()
 
+	// Tu ruta POST
 	r.POST("/verify", middleware.VerifyTokenHandler)
 
-	r.Run(":8082")
+	// Obtén el puerto de la variable de entorno PORT, o usa 8080 para desarrollo local
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8082" // Puerto por defecto para testing local si no hay PORT
+	}
+
+	// Asegúrate de que Gin escuche en el puerto proporcionado por Railway
+	r.Run(":" + port)
 }
