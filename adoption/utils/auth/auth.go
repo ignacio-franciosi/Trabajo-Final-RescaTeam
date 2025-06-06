c962ce1852bff9995054aee516d22cf6ff3ccc09
ignacio-franciosi <2104338@ucc.edu.ar>
2025-06-06 20:45:20 -0300

    cambio de url de funcion

--- adoption/utils/auth/auth.go
+++ adoption/utils/auth/auth.go
@@ -37,8 +37,9 @@ func VerifyTokenAndAuthorize(c *gin.Context, allowAdmin bool, allowOwner bool) (
 
 	reqBody, _ := json.Marshal(TokenRequest{Token: token})
 
-	authUrl := "http://localhost:8082/verify"
+	//authUrl := "http://localhost:8082/verify"
 	//authUrl := "http://rescateam-auth:8082/verify" //docker
+	authUrl := "https://auth-production-1ceb.up.railway.app/verify"
 	resp, err := http.Post(authUrl, "application/json", bytes.NewBuffer(reqBody))
 	if err != nil {
 		fmt.Println("===> Error al llamar al auth:", err)
