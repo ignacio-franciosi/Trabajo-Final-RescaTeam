d579b3a63181face33bbe84001170b95b0d6a157
ignacio-franciosi <2104338@ucc.edu.ar>
2025-06-08 12:44:11 -0300

    imagenes servidas en AWS S3

--- adoption/main.go
+++ adoption/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"github.com/ignacio-franciosi/Trabajo-Final-RescaTeam/adoption/app"
-	"github.com/ignacio-franciosi/Trabajo-Final-RescaTeam/adoption/db"
-	utils "github.com/ignacio-franciosi/Trabajo-Final-RescaTeam/adoption/utils/cache"
+	"adoption/app"
+	"adoption/db"
+	utils "adoption/utils/cache"
+	s3client "adoption/utils/s3"
+	"log"
 )
 
 func main() {
+
+	if err := s3client.InitS3Client(); err != nil {
+		log.Fatalf("Failed to initialize S3 client: %v", err)
+	}
 	utils.InitCache()
 	db.StartDbEngine()
 	app.StartRoute()
