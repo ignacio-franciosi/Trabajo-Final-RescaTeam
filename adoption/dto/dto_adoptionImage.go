aec42c0ccac7630c3f1037fd5dd1c7bcb09c815b
ignacio-franciosi <2104338@ucc.edu.ar>
2025-08-23 19:29:00 -0300

    eliminacion de MS adoption

--- adoption/dto/dto_adoptionImage.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type AdoptionImageDto struct {
-	ImageId        int    `json:"image_id"`
-	AdoptionPostId int    `json:"adoption_post_id"`
-	FilePath       string `json:"file_path"`
-}
-
-type AdoptionImagesDto []AdoptionImageDto
